90dece135d6976c7159da4efaef7d498a805fbca
Ayana Bando <ayanaflax@yahoo.com>
2021-10-22 17:34:48 -0400

    complete dog data class

--- app/src/main/java/com/example/android/thedoggiesaurus/network/DogPhoto.kt
+++ app/src/main/java/com/example/android/thedoggiesaurus/network/DogPhoto.kt
@@ -1,4 +1,16 @@
 package com.example.android.thedoggiesaurus.network
 
-class DogPhoto {
-}
+import com.squareup.moshi.Json
+
+/*
+*   // https://dog.ceo/api/breeds/image/random
+* JSON
+* {
+  "message": "https://images.dog.ceo/breeds/hound-ibizan/n02091244_3042.jpg",
+  "status": "success"
+}*/
+
+class DogPhoto(
+    @Json(name = "message") val messageUrl: String?,
+    @Json(name = "status") val statusResponse: String?
+)
