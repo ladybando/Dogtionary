5a33919625685c14df9994cda12e6bec753b7fdc
Ayana Bando <ayanabando@gmail.com>
2021-10-27 18:41:25 -0400

    Update MainActivity.kt

--- app/src/main/java/com/example/android/thedoggiesaurus/MainActivity.kt
+++ app/src/main/java/com/example/android/thedoggiesaurus/MainActivity.kt
@@ -16,6 +16,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
     }
 }
